27e6b1235ec5db97fefbf730e8d19cc2d81011b9
wangmengen <Zyzx@10086>
2021-12-30 18:04:31 +0800

    项目编写

--- src/main/java/com/wangyunqi/controller/UserController.java
+++ src/main/java/com/wangyunqi/controller/UserController.java
@@ -20,6 +20,18 @@ public class UserController {
     @ApiOperation("用户注册")
     @PostMapping("/sign/in")
     public Result<Object> signIn(@RequestBody @Validated Object dto) {
-        return Result.success("测试成功");
+        return Result.success("测试注册");
+    }
+
+    @ApiOperation("用户登录")
+    @PostMapping("/sign/on")
+    public Result<Object> signOn(@RequestBody @Validated Object dto) {
+        return Result.success("测试登录");
+    }
+
+    @ApiOperation("用户注销")
+    @PostMapping("/sign/out")
+    public Result<Object> signOut(@RequestBody @Validated Object dto) {
+        return Result.success("测试注销");
     }
 }
